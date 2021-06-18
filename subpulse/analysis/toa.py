fixed bug in calculation of z-statistic

subpulse/analysis/toa.py
@@ -141,12 +141,26 @@ def z2search(toas: np.ndarray, errors: np.ndarray, grid: np.ndarray) -> np.ndarr
     np.ndarray
         [description]
     """
-    lc = lightcurve.Lightcurve(toas, np.ones(len(toas)), err=errors, skip_checks=True)
-    z1 = np.empty(grid.size)
+    lc = lightcurve.Lightcurve(toas, np.ones(len(toas)), err=errors)
+    z1 = np.zeros(grid.size, dtype=np.float64)
+    z1p = np.zeros(grid.size, dtype=np.float64)
     lc_time = lc.time
+    import stingray.pulse.pulsar as plsr
     for index in np.arange(0, len(grid), 1):
         phase = pulse_phase(lc_time, grid[index])
         z1[index] = z_n(phase, n=1)
+#        z1p[index] = plsr.z_n(phase, n=1)
+    
+    """ 
+    if (np.array_equal(z1, z1p)):
+        print("SAME!")
+    else:
+        print("NOT SAME :(!")
+        print(z1)
+        print(z1p)
+        1/0
+    """
+    
     return z1
 
 
@@ -226,13 +240,13 @@ def z_n(phase: np.ndarray, n: int = 2, norm: float = 1.0):
     else:
         total_norm = np.sum(normalization)
     phase = phase * 2 * np.pi
-    return 2 / total_norm * statistic(n, phase, normalization)
+    return 2. / total_norm * statistic(n, phase, normalization)
 
 
 @jit(nopython=True)
 def statistic(n, phase, norm):
     """Calculate Z^2 Statistic."""
-    stat = np.empty(n + 1)
+    stat = np.zeros(n + 1, dtype=np.float64)
     for k in range(1, n + 1):
         stat[k - 1] = (
             np.sum(np.cos(k * phase) * norm) ** 2
@@ -345,7 +359,7 @@ def execute(
         simulations, differences, minimum, maximum
     )
     log.debug("Dataset: ✔️")
-    max_z12_power = np.empty(len(toas_mc))
+    max_z12_power = np.zeros(len(toas_mc))
 
     for index in tqdm(
         np.arange(0, len(toas_mc), 1),